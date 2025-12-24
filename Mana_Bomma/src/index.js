7c9b7578b774e6f6c60624248f841992b5e05fdf
TameemMubarak <m.ansarithameem004@gmail.com>
2025-12-24 18:50:18 +0530

    Update index.js

--- Mana_Bomma/src/index.js
+++ Mana_Bomma/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// const API_URL="http://www.omdbapi.com?apikey=8186e430";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
